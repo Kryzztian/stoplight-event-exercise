0bf1bc11eee32005802c0ecb1819fc0de59f0989
Kryzztian <cdkittsteiner@yahoo.com>
2022-11-12 18:25:07 -0600

    Completed the stoplight exercise

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-(function() {
-  'use strict';
+let stopButton = document.querySelector('#stopButton');
+let stopLight = document.querySelector('#stopLight');
+let slowButton = document.querySelector('#slowButton');
+let slowLight = document.querySelector('#slowLight');
+let goButton = document.querySelector('#goButton');
+let goLight = document.querySelector('#goLight');
 
-  // YOUR CODE HERE
-})();
+
+
+stopButton.addEventListener("click", () => {
+  stopLight.classList.toggle("stop")
+});
+
+slowButton.addEventListener("click", () => {
+  slowLight.classList.toggle("slow")
+});
+
+goButton.addEventListener("click", () => {
+  if(goLight.classList.toggle("go")){
+  console.log(`${goButton.textContent} bulb on`)}
+  else {
+    console.log(`${goButton.textContent} bulb off`)
+  }
+});
+
+// This is an efficient way to create a function that will apply each of the arrays values into the webpage.
+function inOut(button){
+  button.addEventListener('mouseenter',() => {
+    console.log(`Entered ${button.textContent} button`)
+  })
+  
+  button.addEventListener('mouseleave', () => {
+    console.log(`Exited ${button.textContent} button`)
+  })
+}
+
+// inOut(stopButton);
+// inOut(slowButton);
+// inOut(goButton);
+
+let arr = [stopButton, slowButton, goButton]
+arr.forEach(inOut)
